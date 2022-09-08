22ec8e8053ee5ab903734414362a1f1ff7da9d83
I66575 <ARJUN.TM@microchip.com>
2022-09-08 17:46:47 +0530

    1) Change in comments : PIM to DIM and Sagitta to Sagita++ 2) Correction in the name of internal amplifier selection

--- mclv-48v-300w-an1292-dspic33ck1024mp710/hal/cmp.h
+++ mclv-48v-300w-an1292-dspic33ck1024mp710/hal/cmp.h
@@ -5,7 +5,7 @@
  * This header file lists interface functions - configuring and enabling the 
  * Comparator Modules and its output
  * 
- * Definitions in this file are for dsPIC33CK256MP508.
+ * Definitions in this file are for dsPIC33CK1024MP710.
  * 
  * Component: HAL - CMP
  * 
